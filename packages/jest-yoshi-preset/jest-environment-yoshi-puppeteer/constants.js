8b39bd4edda0d70d44571d5943ee9b400c746f55
Amit Dahan <amitda@wix.com>
2020-07-01 11:27:28 +0300

    Save puppeteer wsEndpoint in a FRESH temp dir

--- packages/jest-yoshi-preset/jest-environment-yoshi-puppeteer/constants.js
+++ packages/jest-yoshi-preset/jest-environment-yoshi-puppeteer/constants.js
@@ -1,7 +1,11 @@
 const os = require('os');
 const path = require('path');
+const fs = require('fs-extra');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
-module.exports.WS_ENDPOINT_PATH = path.join(DIR, 'wsEndpoint');
-module.exports.IS_DEBUG_MODE = path.join(DIR, 'isDebugMode');
+fs.ensureDirSync(DIR);
+const tempDir = fs.mkdtempSync(`${DIR}${path.sep}`);
+
+module.exports.WS_ENDPOINT_PATH = path.join(tempDir, 'wsEndpoint');
+module.exports.IS_DEBUG_MODE = path.join(tempDir, 'isDebugMode');
